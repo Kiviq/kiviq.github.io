a63808bfb4c02e1a6bbc9fa8b6858e0245db467b
Maciej Mikulski <maciej.mikulski@sarigato.com>
2019-07-04 22:32:37 +0200

    Updates

---
+++ precache-manifest.b14188cd2c7cf33fb40af172e2676fdb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "041275cb35dcd7441505f6cb076f6d65",
+    "url": "/kiviq.github.io/static/media/out2.041275cb.png"
+  },
+  {
+    "revision": "13b531cb5e2c383da16e",
+    "url": "/kiviq.github.io/static/css/main.07e52a66.chunk.css"
+  },
+  {
+    "revision": "918e6169d2a1df233379",
+    "url": "/kiviq.github.io/static/js/runtime~main.918e6169.js"
+  },
+  {
+    "revision": "126ebf7357e8057822d28819228029ab",
+    "url": "/kiviq.github.io/static/media/out3.126ebf73.png"
+  },
+  {
+    "revision": "0fb4752950205bce140b",
+    "url": "/kiviq.github.io/static/js/2.0fb47529.chunk.js"
+  },
+  {
+    "revision": "ca865a8e20ff95a9abd13defdb08b50d",
+    "url": "/kiviq.github.io/static/media/logo.ca865a8e.png"
+  },
+  {
+    "revision": "13b531cb5e2c383da16e",
+    "url": "/kiviq.github.io/static/js/main.13b531cb.chunk.js"
+  },
+  {
+    "revision": "0fc17f85875d5fb66f23d898d9afeb52",
+    "url": "/kiviq.github.io/static/media/lov.0fc17f85.png"
+  },
+  {
+    "revision": "52f5c8a7c9a8dbf921424beda1ddc643",
+    "url": "/kiviq.github.io/static/media/wd.52f5c8a7.png"
+  },
+  {
+    "revision": "24b37c8cdc8776e6dabc6689c0251d7d",
+    "url": "/kiviq.github.io/static/media/me_face.24b37c8c.jpg"
+  },
+  {
+    "revision": "0fb4752950205bce140b",
+    "url": "/kiviq.github.io/static/css/2.0a66ccdc.chunk.css"
+  },
+  {
+    "revision": "9506022599ee781157d9073ed4d30e68",
+    "url": "/kiviq.github.io/index.html"
+  }
+];
